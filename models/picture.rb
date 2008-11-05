11fd6b623b176a02bb54dbf8248847b59119a131
Luca Spiller <luca@deerandspiller.com>
2008-11-05 00:23:36 +0000

    Add more stuff to Picture model to autogenerate file hash

--- models/picture.rb
+++ models/picture.rb
@@ -1,6 +1,26 @@
+require 'digest/sha1'
+
 class Picture < ActiveRecord::Base
 	belongs_to :album
 	
-	validates_presence_of :name, :filename, :hash, :album
+	validates_presence_of :name, :file, :hash, :album
 	validates_associated :album
+	
+	before_create :generate_hash
+	
+	def initialize(file, album)
+		super(:name => file, :file => file, :album => album)
+	end
+	
+	def filepath
+		Pathname.new("#{album.realpath}/#{file}").realpath
+	end
+	
+	def generate_hash
+		self.filehash = Picture.get_hash(filepath)
+	end
+	
+	def self.get_hash(file)
+		Digest::SHA1.hexdigest(File.open(file, 'r').read)
+	end
 end
